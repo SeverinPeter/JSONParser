090bf880a6e2dc5c6fd4a0dd70aca0ced4e2465c
speter <sevi.peter@gmail.com>
2016-12-19 12:59:15 +0100

    lala

--- src/main/java/servlets/ParserServlet.java
+++ src/main/java/servlets/ParserServlet.java
@@ -1,22 +1,30 @@
 package servlets;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
 public class ParserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throw Throwable {
-		PrintWriter() writer = response.getWriter();
-		writer.printline("<h1>Hello</h1>");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello</h1>");
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throw Throwable {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 		
 	}
 
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throw Throwable {
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) {
 	
 	}
 
-	protected void do(HttpServletRequest request, HttpServletResponse response) throw Throwable {
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) {
 	
 	}
 	
